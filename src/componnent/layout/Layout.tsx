70a678d3bad2280b9137b4733e692546f4b5d465
huda nady <hudanady9@gmail.com>
2024-10-20 06:20:24 +0300

    commit

--- src/componnent/layout/Layout.tsx
+++ src/componnent/layout/Layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Nav from '../Nav/Nav'
 
 export default function Layout() {
   return <>
-
+<Nav/>
 
 <div className='container'>
 <Outlet />
